85f9baa311dd1ebc93e429ba2bf1013ef798f512
Paing Hmu Ko <paihmuko95@gmail.com>
2024-07-09 13:29:39 -0600

    prepare for deployment

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import { useState, useEffect } from 'react';
+// component
+import Question from './components/question';
+// data
+import { data } from "./data/questions";
+// MUI
+import { Alert, AppBar, Box, Button, Container, Typography } from '@mui/material';
+// css
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+
+  const [questions, setQuestions] = useState([]);
+  const [answeredCount, setAnsweredCount] = useState(0);
+  const [resetCount, setResetCount] = useState(0);
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const shuffleArray = (arr) => {
+      const array = [...arr];
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+  };
+
+  const handleChange = (question, value) => {
+
+    setQuestions(prevQuestions => 
+      prevQuestions.map(q => q.question === question ? {...q, answer: value} : q)
+    );
+    setAnsweredCount(answeredCount + 1);
+  };
+
+  const handleSubmitClicked = () => {
+    setShowAlert(false);
+    if (questions.filter(x => x.answer !== "").length !== questions.length) {
+      setShowAlert(true);
+    } else {
+      setShowAnswer(true);
+    }
+  }
+
+  const handleResetClicked = () => {
+    console.log('=== Data ===', data);
+    setQuestions(JSON.parse(JSON.stringify(data)));
+    setResetCount(prev => prev + 1);
+    setShowAnswer(false);
+    setShowAlert(false);
+  };
+
+  // useEffects
+  useEffect(() => {
+    setQuestions(JSON.parse(JSON.stringify(
+      shuffleArray(data)
+    )));
+  }, []);
+
+  return (
+    <Container disableGutters maxWidth={'xl'}>
+      <AppBar 
+        position="static" 
+        sx={{ 
+          justifyContent: "space-between", 
+          flexFlow: "row", 
+          padding: "0.5em" 
+        }}
+      >
+          <Typography 
+            variant="h6" 
+            component="div" 
+            sx={{ 
+              marginRight: "1em", 
+              flexGrow: "1" 
+            }
+          }>
+            Answered: {questions.filter(x => x.answer !== "").length} / {questions.length}
+          </Typography>
+          <Button color="inherit" onClick={handleResetClicked}>Reset</Button>
+          <Button color="inherit" onClick={handleSubmitClicked}>Submit</Button>
+      </AppBar>
+      <Box 
+        sx={{ 
+          display: "flex", 
+          flexFlow: "column", 
+          gap: "1em", 
+          overflowY: "auto", 
+          height: "calc(100vh - 100px)", 
+          paddingLeft: "1.5em", 
+          paddingRight: "1.5em", 
+          paddingTop: '1em', 
+          paddingBottom: "1em" 
+        }
+      }>
+        {showAlert && <Alert severity="error">Please answer all the questions.</Alert>}
+        {questions.map((question, key) => <Question
+          id={key + 1}
+          question={question.question}
+          options={question.options}
+          answer={question.answer}
+          correct_answer={question.correct_answer}
+          handleChange={handleChange}
+          key={`${question.id}-${resetCount}`}
+          showAnswer={showAnswer}
+        />)}
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/question.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+// MUI
+import {
+    Avatar,
+    Box,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Radio,
+    RadioGroup,
+} from '@mui/material';
+import { blue, red } from '@mui/material/colors';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
+
+const Question = ({ id, question, options, handleChange, correct_answer, showAnswer }) => {
+
+    const [value, setValue] = useState(null);
+
+    return (
+        <FormControl>
+            <FormLabel sx={{ color: "#000 !important", fontWeight: "700" }}>{`${id}. ${question}`}</FormLabel>
+            <RadioGroup
+                value={value}
+                onChange={(e) => {
+                    setValue(e.target.value);
+                    handleChange(question, e.target.value);
+                }}
+            >
+                {options.map(
+                    (option, index) => <Box 
+                        component={'div'} 
+                        sx={{ display: "flex", flexFlow: "row", alignItems: "center" }}
+                        key={index}
+                    >
+                        <FormControlLabel
+                            value={option} 
+                            control={<Radio />} 
+                            label={option} 
+                            key={index}
+                        />
+                        {showAnswer && option === value && correct_answer !== option && <Avatar sx={{ bgcolor: red[500], width: "20px", height: "20px" }}>
+                            <ClearIcon sx={{ width: "0.5em", height: "0.5em" }} />
+                        </Avatar>}
+                        {showAnswer && correct_answer === option && <Avatar sx={{ bgcolor: blue[500], width: "20px", height: "20px" }}>
+                            <CheckIcon sx={{ width: "0.5em", height: "0.5em" }} />
+                        </Avatar>}
+                    </Box>)}
+            </RadioGroup>
+        </FormControl>
+    )
+};
+
+export default Question;
